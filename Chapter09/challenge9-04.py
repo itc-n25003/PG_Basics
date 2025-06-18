f5c85655182a82ea21733c7c9770c61225f8a65a
itc-n25003 <n25003@std.it-college.ac.jp>
2025-06-18 01:03:17 +0000

    P132 第9章 チャレンジ問題 終了

--- Chapter09/challenge9-04.py
+++ Chapter09/challenge9-04.py
@@ -2,7 +2,7 @@ import csv
 
 list=[["トップ ガン","リスキー ビジネス","マイノリティ レポート"],["タイタニック","ザ レヴェナント","インセプション"],["トレーニング デイ","マン オン ファイア","フライト"]]
 
-with open("chjp.csv","w") as f:
+with open("chjp.csv","w",encoding="utf-8") as f:
     spamwriter = csv.writer(f, delimiter=",")
     for inlist in list:
         spamwriter.writerow(inlist)
